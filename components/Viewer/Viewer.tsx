2b9b31ed2fd812309a6ae8d35734965d36568d23
Mark Baggett <mbagget1@utk.edu>
2022-04-12 13:04:06 -0400

    Remove title and badge.

--- components/Viewer/Viewer.tsx
+++ components/Viewer/Viewer.tsx
@@ -4,5 +4,18 @@ const CloverIIIF = dynamic(() => import("@samvera/clover-iiif"), {
   ssr: false,
 });
 
-const Viewer = ({ manifestId }) => <CloverIIIF manifestId={manifestId} />;
+const options = {
+  showTitle: false,
+  showIIIFBadge: false
+}
+
+const customTheme = {
+  colors: {
+    primary: "#FFFFFF",
+    primaryMuted: "#FFFFFF",
+    primaryAlt: "#FFFFFF"
+  }
+};
+
+const Viewer = ({ manifestId }) => <CloverIIIF manifestId={manifestId} options={options} customTheme={customTheme} />;
 export default Viewer;
